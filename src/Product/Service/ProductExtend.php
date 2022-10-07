a84cd2546dd40a8a9e9e755452b113b351ed02a6
hkreuter <heike.reuter@oxid-esales.com>
2022-10-07 16:15:34 +0200

    Update example as strrev caused trouble with mb string title

--- src/Product/Service/ProductExtend.php
+++ src/Product/Service/ProductExtend.php
@@ -6,33 +6,29 @@ use OxidEsales\GraphQL\Storefront\Product\DataType\Product as ProductDataType;
 use TheCodingMachine\GraphQLite\Annotations\ExtendType;
 use TheCodingMachine\GraphQLite\Annotations\Field;
 
-use function sprintf;
-use function strrev;
-
 /**
  * @ExtendType(class=ProductDataType::class)
  */
 final class ProductExtend
 {
     /**
-     * Wrapper for different wording
+     * Wrapper for different wording - if you insist the field needs a better suited name ;)
      *
-     * @Field()
+     * @Field(name="OxAcItemNumber")
      */
     public function itemNumber(ProductDataType $product): string
     {
-        return (string) $product->getEshopModel()->getFieldData('oxartnum');
+        return (string) $product->getSku();
     }
 
     /**
-     * Mocks a custom field by using and changing a native field.
+     * Fetch raw data from the shop model and compile your own information from that.
      *
-     * @Field()
+     * @Field(name="OxAcProductIdentifier")
      */
-    public function subtitle(ProductDataType $product): string
+    public function productIdentifier(ProductDataType $product): string
     {
-        $title = $product->getEshopModel()->getFieldData('oxtitle');
-        $shuffledTitle = strrev($title);
-        return sprintf('%s <-> %s', $title, $shuffledTitle);
+        return (string) $product->getEshopModel()->getRawFieldData('oxartnum') . '--' .
+            (string) $product->getEshopModel()->getRawFieldData('oxtitle');
     }
 }
